f1fde4169de696209fd854ba0927c16502f1d659
Илья Тупицин <ilhatupitsin@yandex.ru>
2023-11-20 20:38:00 +0300

    Добавьте файлы проекта.

---
+++ обфускатор/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+class Program
+{
+    static void Main()
+    {
+        // Чтение кода из файла
+        string path = "C:\\Users\\Пользователь\\Desktop\\кликер и таймер.txt";
+        string inputCode = File.ReadAllText(path);
+
+        // Удаляем лишние пробелы и символы перехода на новую строку
+        inputCode = Regex.Replace(inputCode, @"\s+", " ");
+        // Удаляем комментарии
+        //   string compressedCode = Regex.Replace(inputCode, @"\/\*.*?\*\/", "", RegexOptions.Singleline);
+          string compressedCode = Regex.Replace(inputCode, @"//.*$", "", RegexOptions.Singleline);
+        // Заменяем имя класса
+        inputCode = inputCode.Replace("Program", "новое_имя_класса");
+
+        // Заменяем имя файла
+        inputCode = inputCode.Replace("кликер и таймер", "новое_имя_файла");
+
+        // Заменяем имя конструктора
+        inputCode = inputCode.Replace("старое_имя_конструктора", "новое_имя_конструктора");
+        // Находим все идентификаторы в коде
+        var identifiers = Regex.Matches(inputCode, @"\b\w+\b")
+                               .OfType<Match>()
+                               .Select(m => m.Value)
+                               .Distinct()
+                               .ToArray();
+
+        // Заменяем идентификаторы на односимвольные или двухсимвольные имена 
+        for (int i = 0; i < identifiers.Length; i++)
+        {
+            string replacement = i < 26 ? ((char)('a' + i)).ToString() : "var" + (i - 26 + 1);
+            inputCode = Regex.Replace(inputCode, $@"\b{identifiers[i]}\b", replacement);
+        }
+        // Вывод измененного кода
+        Console.WriteLine("Измененный код:");
+        Console.WriteLine(inputCode);
+        Console.WriteLine( "код без коментов:");
+        Console.WriteLine(compressedCode);
+    }
+}
